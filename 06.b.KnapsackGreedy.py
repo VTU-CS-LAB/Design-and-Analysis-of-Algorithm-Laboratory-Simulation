40d09b262c580c736bb3c7fa09ada32764b2febe
Yash Vora <majinsparks@gmail.com>
2018-04-20 18:16:51 +0530

    .

--- 06.b.KnapsackGreedy.py
+++ 06.b.KnapsackGreedy.py
@@ -21,11 +21,15 @@ def fill():
         if currentWeight + weight[item] <= capacity:
             currentWeight += weight[item]
             currentProfit += float(profit[item])
+            solnVector[item] = 1
             ratio[item] = 0
         else:
             currentProfit += float(ratio[item]) * float((capacity - currentWeight))
+            solnVector[item] = (capacity - currentWeight) / weight[item]
             break
     print("\nMaximum profit is: %d" %currentProfit)
+    print("Solution Vector:", solnVector)
+    input()
 
 
 if __name__ == '__main__':
@@ -34,6 +38,7 @@ if __name__ == '__main__':
     weight = []
     profit = []
     ratio = []
+    solnVector = [0] * n
 
     print("Enter weights of items")
     for i in range(n):
